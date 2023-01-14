a7ac55259ef13d573c5eb44a2d520f124f467757
aldom <aldom@ALDODL>
2023-01-14 11:36:22 -0400

    Add project files.

---
+++ ElephantMan/Elephant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElephantMan
+{
+    internal class Elephant
+    {
+        public int earSize;
+        public string name;
+
+        public void  WhoAmI()
+        {
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My ears are " + earSize + " inches tall.");
+
+        }
+
+    }
+}
---
+++ ElephantMan/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ElephantMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { name = "Lucinda", earSize = 33 };
+            Elephant lloyd = new Elephant() { earSize = 40, name = "Lloyd" };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+
+                if (input == '1')
+                {
+                    Console.WriteLine("You pressed " + input);
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                if (input == '2') {
+                    Console.WriteLine("You pressed " + input);
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI(); }
+                if (input == '3')
+                {
+                    Console.WriteLine("You pressed " + input);
+                    Elephant saver;
+                    saver = lucinda;
+                    lucinda = lloyd;
+                    lloyd = saver;
+                    Console.WriteLine("References have been swapped");
+                }
+            }
+        }
+    }
+}   
